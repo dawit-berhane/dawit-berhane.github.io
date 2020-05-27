a73da5f1d2b0fcc66ff4327d604cf1b05d03f33a
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-27 00:16:01 -0500

    added text docorator to decoratemytext.js

--- decorateMyText/Text Decorator_files/decoratemytext.js
+++ decorateMyText/Text Decorator_files/decoratemytext.js
@@ -4,5 +4,19 @@ function biggerDecoration(){
     
 }
 
-
+function onChange(){
+    var checkBoxStatus = document.getElementById('checkbox');
+    var textElement = document.getElementById('text');
+    if(checkBoxStatus.checked){
+    
+    textElement.style.fontWeight = 'bold';
+    textElement.style.color = 'green';
+    textElement.style.textDecorationLine = 'underline';
+   }
+   if(!checkBoxStatus.checked){
+    textElement.style.fontWeight = 'normal';
+    textElement.style.color = 'black';
+    textElement.style.textDecorationLine = 'none';
+   }
+}
 

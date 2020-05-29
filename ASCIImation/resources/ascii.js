8c05a527a059dbc96dea19f7adbbd5b13848a024
Dawit Berhane <dawit.berhane711@gmail.com>
2020-05-29 18:04:15 -0500

    control and display added to html file

---
+++ ASCIImation/resources/ascii.js
@@ -0,0 +1,17 @@
+let startBtn;
+let stopBtn;
+
+let slctanimation;
+let slctsize;
+
+let turbospeed;
+
+let delay;
+
+
+
+
+startBtn = document.getElementById("startBtn");
+stopBtn = document.getElementById("stopBtn");
+
+

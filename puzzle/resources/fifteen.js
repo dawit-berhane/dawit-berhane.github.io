29c8630ed47239c2a0470dfb428230625f77ca8d
Dawit Berhane <dawit.berhane711@gmail.com>
2020-06-05 22:29:19 -0500

    added hover and move to puzzle

--- puzzle/resources/fifteen.js
+++ puzzle/resources/fifteen.js
@@ -1,15 +1,21 @@
+let openTile = {
+    x: 300,
+    y: 300
+}
 $(function(){
     initialPosition();
+    $("#puzzlearea div").hover(isMovable);
+    $("#puzzlearea div").click(move);
 });
 
 
 function initialPosition(){
     $("#puzzlearea").children().each(function(){
         let num = this.innerHTML - 1;
-        console.log(num);
+       
         let x = ((num%4)*100);
         let y = (Math.floor(num/4)*100);
-        console.log(x, y);
+       
         $(this).addClass('puzzlepiece');
         $(this).css({
             "left": x+"px",
@@ -17,9 +23,48 @@ function initialPosition(){
             "background-image": "url('./resources/background.jpg')",
             "background-position": -x+"px" +" "+(-y)+"px"
         });
-        $(this).x = x;
-        $(this).y = y;
+        $(this).data('pos',{'x':x, 'y':y});
+        //console.log($(this).data('pos').x);
         
     });
 }
 
+function isMovable(current){
+    console.log(current);
+    current = $(this);
+    let isMovable = false;
+    let divX = current.data('pos').x;
+    let divY = current.data('pos').y;
+    let openX = parseInt(openTile.x);
+    let openY = parseInt(openTile.y);
+    let distance = Math.abs((divX-openX) + (divY-openY));
+    if(distance == 100){
+        current.addClass('movablepiece');
+        isMovable = true;
+    }
+    return isMovable;
+}
+
+function highlight(){
+    $(this).addClass('movablepiece');
+}
+
+function move(){
+    const current = $(this); 
+    if(isMovable(current)){
+    let divX = current.data('pos').x;
+    let divY = current.data('pos').y;
+    let openX = parseInt(openTile.x);
+    let openY = parseInt(openTile.y);
+    let distance = Math.abs((divX-openX) + (divY-openY));
+    current.css('left', function(idx, old){
+        return (openX)+'px';
+    });
+    current.css('top', function(idx, old){
+        return (openY)+'px';
+    });
+    }
+    
+    
+   
+}
